37f1117423d5a25ca1600e5ec8816a7a26f0ed96
Elijah Smith <elijahsmith.m@gmail.com>
2022-03-06 00:52:44 -0600

    add nav bar, spotify button, fix photo size

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const h2 = document.createElement('h2');
 h2.textContent = 'This content added by JavaScript-- See us live';
 
 document.querySelector('body').appendChild(h2);
+
+const hamburger = document.querySelector(".three_line");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
