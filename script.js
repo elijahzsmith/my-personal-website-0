55f18416fabe7f19ebdc26f75326250e7dbc71fc
Elijah Smith <elijahsmith.m@gmail.com>
2022-03-06 13:35:47 -0600

    Finish Website--fixed navigation menu, commented code, rounded off some edges, would like to come back to beef it up more in the coming days.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // const navigationBar = querySelector('.navbar');
 // navigationBar.
 
-
+//adds "see us live" text to index.html body using js
 const h2 = document.createElement('h2');
 h2.textContent = 'See us live (JS)';
 
 document.querySelector('body').appendChild(h2);
 
+//adds three line menu to nav bar
 const hamburger = document.querySelector(".three_line");
 const navMenu = document.querySelector(".nav-menu");
 
+//adds opening and closing functionality to menu
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
@@ -26,4 +28,5 @@ function closeMenu() {
     navMenu.classList.remove("active");
 }
 
-// add event listeners and modify some html and css using js
+
+
